534ba6f9892669638b54a55be888ca592589970f
Sebastian-14 <sebas14121999@gmail.com>
2020-07-20 01:33:58 -0500

    condicionales, dark mode

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -1,8 +1,15 @@
 <script>
     let someText = 'Frontend Developer FullStack';
     let count = 0;
+    let styles = { darkMode: false }
+
     function handleCount() {
         count += 1
+    }
+
+    function toggle() {
+        styles.darkMode = !styles.darkMode
+        window.document.body.classList.toggle("dark-mode")
     }
 </script>
 
@@ -14,10 +21,21 @@
 </style>
 
 <div class="About">
-    <p>
-        { someText }
-    </p>
+
+    { #if !styles.darkMode }
+        <p>
+            { someText }
+        </p>
+    { :else }
+        <p>
+            <span>Hello DarkMode</span>
+        </p>
+    { /if }
+
     <button on:click={ handleCount }>
         Click { count === 0 ? '' : count }
+    </button>
+    <button on:click={ toggle }>
+        DarkMode
     </button>
 </div>
